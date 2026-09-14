bbe8ac3602f64411010c7d802da2b06875f4d8a2
diazsophie91-hash <diazsophie91@gmail.com>
2026-09-14 12:17:29 +0200

    Delete app directory

--- app/public/wp-content/themes/nature-reiki/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Index principal du theme.
- *
- * Sert de fallback si aucun template de page ne correspond.
- * Renvoie vers la page d'accueil statique.
- */
-
-get_template_part( 'front-page' );
